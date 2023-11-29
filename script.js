bf99da9076ee8a0238d2c440b7de898c67cace9f
EllaRMartin <martin37@lancaster.ac.uk>
2023-11-29 02:33:32 +0000

    Added event listeners

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getScore(total, current)
+function getScrabbleScore(total, current)
 { 
     let i = current.charCodeAt(0) - 65;
     const scrabble = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1];
@@ -10,13 +10,9 @@ function getScore(total, current)
 function displayCodel(score, i)
 {
     let colours = ["red", "yellow", "green", "blue", "purple"];
-    
+    var codelWidth = 10;
     ctx().fillStyle = colours[score%colours.length];
-    ctx().fillRect(10*i, 10, 10, 10);
-}
-function ctx()
-{
-    return document.getElementById("myCanvas").getContext("2d");
+    ctx().fillRect(codelWidth*i, codelWidth, codelWidth, codelWidth);
 }
 function getCanvasSize(noCodels)
 {
@@ -25,16 +21,53 @@ function getCanvasSize(noCodels)
 function getScores()
 {
     let scores = [];
-    let str = document.getElementById("input").value;
+    let str = document.getElementById("beatnikInput").value;
+
     let words = str.split(" ");
     document.getElementById("output").innerHTML = JSON.stringify(words);
     
     words.forEach(word => { 
-        scores.push(word.toUpperCase().split("").reduce(getScore,0));
-        document.getElementById("check").innerHTML = JSON.stringify(scores);
+        scores.push(word.toUpperCase().split("").reduce(getScrabbleScore,0));
+        document.getElementById("output").innerHTML = JSON.stringify(scores);
     });
     
 
-    scores.forEach(displayCodel);
-    
+    repaint(scores)
 }
+function repaint(scores)
+{
+    
+    scores.forEach(score,i => {
+        displayCodel(score,i)
+    });
+}
+function setCanvasSize(){
+    var canvas = document.getElementById("pietCanvas");
+    canvas.width = window.innerWidth/2;
+    canvas.height = window.innerHeight;
+
+}
+function setBeatnikInputBoxSize(){
+    var beatBox = document.getElementById("beatnikInput");
+    beatBox.cols = window.innerWidth/2;
+    beatBox.rows = window.innerHeight;
+}
+function init()
+{
+    //var canvas = document.getElementById("pietCanvas");
+    //var ctx = canvas.getContext("2d");
+    //initialize global variables with let
+
+    //document.getElementById("pietCanvas").addEventListener("resize",setCanvasSize);
+    document.getElementById("beatnikInput").addEventListener("resize",setBeatnikInputBoxSize);
+    document.getElementById("beatnikInput").addEventListener("input",getScores);
+
+    setCanvasSize();
+    //const { width, height } = canvas.getBoundingClientRect();
+    //document.getElementById("beatnikInput").value = width;
+
+
+}
+
+
+window.onload = init;
