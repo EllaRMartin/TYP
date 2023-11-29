6e1a63f7f764ac85bb2ba29b8b70c5bb4a6facef
EllaRMartin <martin37@lancaster.ac.uk>
2023-11-29 04:19:33 +0000

    Hex colours, dark and light colours

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getScrabbleScore(total, current)
 { 
     let i = current.charCodeAt(0) - 65;
-    const scrabble = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1];
+    const scrabble = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
     
     if(scrabble[i]) return total + scrabble[i]; 
     else return total;
@@ -23,15 +23,18 @@ function getScores()
 }
 function repaint(scores)
 {
+                    // red     yellow    green      cyan      blue     magenta
+    let colours = [["#C00000","#C0C000","#00C000","#00C0C0","#0000C0","#C000C0"],//dark colours
+                   ["#FF0000","#FFFF00","#00FF00","#00FFFF","#0000FF","#FF00FF"],//bright colours
+                   ["#FFC0C0","#FFFFC0","#C0FFC0","#C0FFFF","#C0C0FF","#FFC0FF"]]//light colours
 
-    let colours = ["red","yellow","green","cyan","blue","magenta"]
     let canvas = document.getElementById("pietCanvas")
     let ctx = canvas.getContext("2d")
     let codelWidth = canvas.width/scores.length;
     //document.getElementById("output").innerHTML = JSON.stringify(colours[scores[0]]);
     scores.forEach((score, i)=>{
-        if(score ==0) fillStyle = "white";
-        else ctx.fillStyle = colours[score%colours.length]
+        if(score==0) fillStyle = "#FFFFFF";
+        else ctx.fillStyle = colours[score%colours.length][score%colours[0].length]
         ctx.fillRect(i*codelWidth,0,codelWidth,codelWidth);
     });
     
